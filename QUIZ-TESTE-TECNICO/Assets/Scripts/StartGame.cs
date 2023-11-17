a30fcbf0bd6516d69a2474a624866fbb72e6b11e
Leonardo Santos <leonardo.santos574@hotmail.com>
2023-11-17 10:49:31 -0300

    adjust layout QuizScene

--- QUIZ-TESTE-TECNICO/Assets/Scripts/StartGame.cs
+++ QUIZ-TESTE-TECNICO/Assets/Scripts/StartGame.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
@@ -23,4 +22,6 @@ public class StartGame : MonoBehaviour
         SceneManager.LoadScene(1);
 
     }
+
+
 }
